4437b41bb6b0a98f3763e9b32e940ae951c01a98
Dr Yakes <98264362+DR-Yakes@users.noreply.github.com>
2022-09-15 21:30:00 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,23 +9,22 @@ void print_triangle(int size)
 	if (size <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
-		int a, b;
+		int i, j;
 
-		for (a = 1; a <= size; a++)
+		for (i = 1; i <= size; i++)
 		{
-			for (b = a; b < size; b++)
+			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
 			}
-			
-			for (b = 1; b <= a; b++)
+
+			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
-			
+
 			_putchar('\n');
 		}
 	}
